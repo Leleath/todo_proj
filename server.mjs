97f5167266a6591f7b79c3f1766e93e577e3924f
Leleath <qleleath@gmail.com>
2022-09-06 01:52:45 +1000

    1

--- server.mjs
+++ server.mjs
@@ -1,6 +1,12 @@
-import { createServer } from 'http';
+// import { createServer } from 'http';
+import express from "express";
+let app = express();
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+app.get("/api/users", function(req, res) {
+  console.log("Hello");
+  res.send("working");
+});
+
+app.listen(process.env.PORT, "127.0.0.1", () => {
+  console.log("api server started");
+});
