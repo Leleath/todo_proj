966c175ab2732a76e516eca39914438bc128ddc1
Leleath <qleleath@gmail.com>
2022-09-06 17:13:03 +1000

    cors

--- server.mjs
+++ server.mjs
@@ -9,8 +9,10 @@ let DEBUG = false;
 
 var corsOptions = {
   origin: 'http://localhost:3000',
+  credentials:true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+app.use(cors(corsOptions));
 
 let port;
 if (DEBUG) {
