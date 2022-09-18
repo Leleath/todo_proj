04743f69967c6a57583f0d41afae3d22661d9a10
Leleath <qleleath@gmail.com>
2022-09-18 18:00:00 +1000

    cors

--- routes.js
+++ routes.js
@@ -11,21 +11,6 @@ const TOKENKEY = process.env.TOKENKEY;
 
 module.exports = function(app, db) {
 
-    function GetJWToken(result) {
-        let head = Buffer.from(
-            JSON.stringify({ alg: 'HS256', typ: 'jwt' })
-        ).toString('base64');
-
-        let body = Buffer.from(JSON.stringify(result)).toString('base64');
-
-        let signature = crypto
-            .createHmac('SHA256', TOKENKEY)
-            .update(`${head}.${body}`)
-            .digest('base64');
-
-        return {"head": head, "body": body, "signature": signature};
-    }
-
     // api middleware
     // app.use((req, res, next) => {
     //     req.headers['From-Middleware'] = 1;
@@ -51,12 +36,21 @@ module.exports = function(app, db) {
 
             if (result) {
                 if (result[0].password == password) {
-                    let data = GetJWToken(result[0]);
+                    let head = Buffer.from(
+                        JSON.stringify({ alg: 'HS256', typ: 'jwt' })
+                    ).toString('base64');
+            
+                    let body = Buffer.from(JSON.stringify(result[0])).toString('base64');
+            
+                    let signature = crypto
+                        .createHmac('SHA256', TOKENKEY)
+                        .update(`${head}.${body}`)
+                        .digest('base64');
 
                     return res.status(200).json({
                         id: result[0].id,
                         username: result[0].username,
-                        token: `${data.head}.${data.body}.${data.signature}`,
+                        token: `${head}.${body}.${signature}`,
                     })
                 } else {
                     return res.status(400).json({message: "Incorrect login or password"});
