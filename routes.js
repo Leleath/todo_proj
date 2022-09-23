7c9f8e051759ce8bc2a6a4a557dd760062e2166f
Leleath <qleleath@gmail.com>
2022-09-24 00:09:10 +1000

    r6

--- routes.js
+++ routes.js
@@ -15,22 +15,22 @@ module.exports = function(app) {
 
     app.get(APIURL + '/users', auth, (req, res) => {
         res.json({message:'users'});
-        let sql = 'SELECT * FROM users';
-        console.log("get users")
-        db.query(sql, (err, result, fields) => {
-            if (err) throw err;
+        // let sql = 'SELECT * FROM users';
+        // console.log("get users")
+        // db.query(sql, (err, result, fields) => {
+        //     if (err) throw err;
             
-            console.log("get users list")
+        //     console.log("get users list")
 
-            return res.status(200).json(result);
-        });
+        //     return res.status(200).json(result);
+        // });
     });
     
     app.post(APIURL + '/users/login', bodyParser, async (req, res) => {
         let infoUser = req.body;
   
         try{
-            let user = await Parse.User.logIn(infoUser.usernameLogin, infoUser.passwordLogin);
+            let user = await Parse.User.logIn(infoUser.username, infoUser.password);
             let head = Buffer.from(
                 JSON.stringify({ alg: 'HS256', typ: 'jwt' })
             ).toString('base64');
@@ -56,8 +56,8 @@ module.exports = function(app) {
         let infoUser = req.body;    
         let user = new Parse.User();
       
-        user.set("username", infoUser.usernameRegister);
-        user.set("password", infoUser.passwordRegister);
+        user.set("username", infoUser.username);
+        user.set("password", infoUser.password);
 
         try{
             await user.signUp();
