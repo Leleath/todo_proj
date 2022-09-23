e87f01a0ec9fcc64da08da692d855a6b034aa4db
Leleath <qleleath@gmail.com>
2022-09-23 23:40:03 +1000

    r2

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 const auth = require('./middleware/auth');
-// const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const bodyParser = require('body-parser').json();
 const md5 = require('md5');
-require('dotenv').config();
 
 const APIURL = '/api';
 const MD5KEY = process.env.MD5KEY;
@@ -11,81 +11,76 @@ const TOKENKEY = "todo";
 
 module.exports = function(app, db) {
 
-    // api middleware
-    // app.use((req, res, next) => {
-    //     req.headers['From-Middleware'] = 1;
-    //     next();
-    // })
-
     app.get(APIURL + '/users', auth, (req, res) => {
-        let sql = 'SELECT * FROM users';
-        console.log("get users")
-        db.query(sql, (err, result, fields) => {
-            if (err) throw err;
+        res.json({message:'users'});
+        // let sql = 'SELECT * FROM users';
+        // console.log("get users")
+        // db.query(sql, (err, result, fields) => {
+        //     if (err) throw err;
             
-            console.log("get users list")
+        //     console.log("get users list")
 
-            return res.status(200).json(result);
-        });
+        //     return res.status(200).json(result);
+        // });
     });
     
     app.post(APIURL + '/users/login', bodyParser, (req, res) => {
-        const username = req.body.username;
-        const password = md5(req.body.password + MD5KEY);
+        // const username = req.body.username;
+        // const password = md5(req.body.password + MD5KEY);
 
-        let sql = 'SELECT * FROM users WHERE username = "' + username + '"';
-        db.query(sql, (err, result, fields) => {
-            if (err) throw err;
+        // let sql = 'SELECT * FROM users WHERE username = "' + username + '"';
+        // db.query(sql, (err, result, fields) => {
+        //     if (err) throw err;
 
-            if (result) {
-                if (result[0].password == password) {
-                    let head = Buffer.from(
-                        JSON.stringify({ alg: 'HS256', typ: 'jwt' })
-                    ).toString('base64');
+        //     if (result) {
+        //         if (result[0].password == password) {
+        //             let head = Buffer.from(
+        //                 JSON.stringify({ alg: 'HS256', typ: 'jwt' })
+        //             ).toString('base64');
             
-                    let body = Buffer.from(JSON.stringify(result[0])).toString('base64');
+        //             let body = Buffer.from(JSON.stringify(result[0])).toString('base64');
             
-                    let signature = crypto
-                        .createHmac('SHA256', TOKENKEY)
-                        .update(head + '.' + body)
-                        .digest('base64');
+        //             let signature = crypto
+        //                 .createHmac('SHA256', TOKENKEY)
+        //                 .update(head + '.' + body)
+        //                 .digest('base64');
 
-                    return res.status(200).json({
-                        id: result[0].id,
-                        username: result[0].username,
-                        token: `${head}.${body}.${signature}`,
-                    })
-                } else {
-                    return res.status(400).json({message: "Incorrect login or password"});
-                }
-            } else {
-                return res.status(400).json({message: "user not found"});
-            }
-        })
+        //             return res.status(200).json({
+        //                 id: result[0].id,
+        //                 username: result[0].username,
+        //                 token: `${head}.${body}.${signature}`,
+        //             })
+        //         } else {
+        //             return res.status(400).json({message: "Incorrect login or password"});
+        //         }
+        //     } else {
+        //         return res.status(400).json({message: "user not found"});
+        //     }
+        // })
     });
 
     app.post(APIURL + '/users/signup', bodyParser, (req, res, next) => {
-        let username = req.body.username;
-        let password = md5(req.body.password + MD5KEY);
-        let sql;
+    //     let username = req.body.username;
+    //     let password = md5(req.body.password + MD5KEY);
+    //     let sql;
 
-        // Поиск в БД одиннакового имя пользователя
-        sql = 'SELECT * FROM users WHERE username = "' + username + '"';
-        db.query(sql, (err, result, fields) => {
-            // Остановить, если ошибка
-            if (err) throw err;
+    //     // Поиск в БД одиннакового имя пользователя
+    //     sql = 'SELECT * FROM users WHERE username = "' + username + '"';
+    //     db.query(sql, (err, result, fields) => {
+    //         // Остановить, если ошибка
+    //         if (err) throw err;
             
-            if (result.length > 0) {
-                return res.status(401).json({ message: "User exists"});
-            } else {
-                sql = 'INSERT INTO users (username, password, role) VALUES ("' + username + '","' + password + '","User")';
-                db.query(sql, (err, result, fields) => {
-                    if (err) throw err;
+    //         if (result.length > 0) {
+    //             return res.status(401).json({ message: "User exists"});
+    //         } else {
+    //             sql = 'INSERT INTO users (username, password, role) VALUES ("' + username + '","' + password + '","User")';
+    //             db.query(sql, (err, result, fields) => {
+    //                 if (err) throw err;
         
-                    return res.status(200).json({message: "User created"});
-                });
-            }
-        })
+    //                 return res.status(200).json({message: "User created"});
+    //             });
+    //         }
+    //     })
     });
 
     console.log('api loaded');
--- server.js
+++ server.js
@@ -5,58 +5,22 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// const Parse = require('parse');
-// Parse.initialize("XogTF2OtNScZ2orqYj7at7VantSZIq9RvlTKiC1B", "SnZhZuzuxU0kjWYD4FzE8uVzRzRFnpGg16aOdzO3");
-
 // без понятия что это, но это помогает
 const cors = require('cors');
 app.use(cors({
   origin: '*'
 }));
 
-// Присваивания порта по стенду проекта
-let port = (process.env.DEBUG == "dev") ? 3000 : process.env.PORT;
+let port = process.env.PORT;
 console.log(port);
-
-
-app.get('/', (req, res) => {
-  res.json({message:'lul'})
-})
 
 app.listen(port, (err) => {
   // Остановить, если ошибка
   if (err) throw err;
 
-  // require('./routes.js')(app, db);
+  // Подключение API
+  // Подключение API
+    require('./routes.js').default(app, db);
 
   console.log('api server started');
 })
-
-
-// Создание подключения к БД
-// let db = mysql.createConnection({
-//   host: process.env.MYSQLHOST,
-//   user: process.env.MYSQLUSER,
-//   database: process.env.MYSQLDATABASE,
-//   password: process.env.MYSQLPASSWORD,
-//   port: process.env.MYSQLPORT
-// });
-
-// (проверка) Подключение к БД
-// db.connect(function(err) {
-//   // Остановить, если ошибка
-//   if (err) throw err;
-
-//   console.log('DB connected');
-
-//   // Подключение файла с API
-//   require('./routes.js')(app, db);
-
-//   // Создание прослушки сервера
-//   app.listen(port, (err) => {
-//     // Остановить, если ошибка
-//     if (err) throw err;
-
-//     console.log('api server started');
-//   })
-// })
