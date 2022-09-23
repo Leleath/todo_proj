edcf1d3c03a4773cb472c04ffec3ee795b953fb1
Leleath <qleleath@gmail.com>
2022-09-24 00:06:12 +1000

    r5

--- routes.js
+++ routes.js
@@ -47,68 +47,24 @@ module.exports = function(app) {
                 username: user.username,
                 token: `${head}.${body}.${signature}`,
             })
-            // res.render('index', { loginMessage: "User logged!", RegisterMessage: '', typeStatus: "success",  infoUser: infoUser });
         } catch (error){
             return res.status(400).json({message: "user not found"});
-            // res.render('index', { loginMessage: error.message, RegisterMessage: '', typeStatus: "danger",  infoUser: infoUser});
         }
-
-        // const username = req.body.username;
-        // const password = md5(req.body.password + MD5KEY);
-
-        // let sql = 'SELECT * FROM users WHERE username = "' + username + '"';
-        // db.query(sql, (err, result, fields) => {
-        //     if (err) throw err;
-
-        //     if (result) {
-        //         if (result[0].password == password) {
-        //             let head = Buffer.from(
-        //                 JSON.stringify({ alg: 'HS256', typ: 'jwt' })
-        //             ).toString('base64');
-            
-        //             let body = Buffer.from(JSON.stringify(result[0])).toString('base64');
-            
-        //             let signature = crypto
-        //                 .createHmac('SHA256', TOKENKEY)
-        //                 .update(head + '.' + body)
-        //                 .digest('base64');
-
-        //             return res.status(200).json({
-        //                 id: result[0].id,
-        //                 username: result[0].username,
-        //                 token: `${head}.${body}.${signature}`,
-        //             })
-        //         } else {
-        //             return res.status(400).json({message: "Incorrect login or password"});
-        //         }
-        //     } else {
-        //         return res.status(400).json({message: "user not found"});
-        //     }
-        // })
     });
 
-    app.post(APIURL + '/users/signup', bodyParser, (req, res, next) => {
-    //     let username = req.body.username;
-    //     let password = md5(req.body.password + MD5KEY);
-    //     let sql;
+    app.post(APIURL + '/users/signup', bodyParser, async (req, res, next) => {
+        let infoUser = req.body;    
+        let user = new Parse.User();
+      
+        user.set("username", infoUser.usernameRegister);
+        user.set("password", infoUser.passwordRegister);
 
-    //     // Поиск в БД одиннакового имя пользователя
-    //     sql = 'SELECT * FROM users WHERE username = "' + username + '"';
-    //     db.query(sql, (err, result, fields) => {
-    //         // Остановить, если ошибка
-    //         if (err) throw err;
-            
-    //         if (result.length > 0) {
-    //             return res.status(401).json({ message: "User exists"});
-    //         } else {
-    //             sql = 'INSERT INTO users (username, password, role) VALUES ("' + username + '","' + password + '","User")';
-    //             db.query(sql, (err, result, fields) => {
-    //                 if (err) throw err;
-        
-    //                 return res.status(200).json({message: "User created"});
-    //             });
-    //         }
-    //     })
+        try{
+            await user.signUp();
+            res.status(200).json({message: "User created"});
+        } catch (error) {
+            res.status(400).json({message: "error"})
+        }
     });
 
     console.log('api loaded');
