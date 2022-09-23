734a12a97ee52d469f62e399e2b1a58a10b20d6a
Leleath <qleleath@gmail.com>
2022-09-23 23:35:40 +1000

    reload

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 app.use(express.json());
 
+// const Parse = require('parse');
+// Parse.initialize("XogTF2OtNScZ2orqYj7at7VantSZIq9RvlTKiC1B", "SnZhZuzuxU0kjWYD4FzE8uVzRzRFnpGg16aOdzO3");
+
 // без понятия что это, но это помогает
 const cors = require('cors');
-// var corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentials:true,
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
 app.use(cors({
   origin: '*'
 }));
@@ -20,30 +18,45 @@ app.use(cors({
 let port = (process.env.DEBUG == "dev") ? 3000 : process.env.PORT;
 console.log(port);
 
-// Создание подключения к БД
-let db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  database: process.env.MYSQLDATABASE,
-  password: process.env.MYSQLPASSWORD,
-  port: process.env.MYSQLPORT
-});
 
-// (проверка) Подключение к БД
-db.connect(function(err) {
+app.get('/', (req, res) => {
+  res.json({message:'lul'})
+})
+
+app.listen(port, (err) => {
   // Остановить, если ошибка
   if (err) throw err;
 
-  console.log('DB connected');
+  // require('./routes.js')(app, db);
 
-  // Подключение файла с API
-  require('./routes.js')(app, db);
-
-  // Создание прослушки сервера
-  app.listen(port, (err) => {
-    // Остановить, если ошибка
-    if (err) throw err;
-
-    console.log('api server started');
-  })
+  console.log('api server started');
 })
+
+
+// Создание подключения к БД
+// let db = mysql.createConnection({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   database: process.env.MYSQLDATABASE,
+//   password: process.env.MYSQLPASSWORD,
+//   port: process.env.MYSQLPORT
+// });
+
+// (проверка) Подключение к БД
+// db.connect(function(err) {
+//   // Остановить, если ошибка
+//   if (err) throw err;
+
+//   console.log('DB connected');
+
+//   // Подключение файла с API
+//   require('./routes.js')(app, db);
+
+//   // Создание прослушки сервера
+//   app.listen(port, (err) => {
+//     // Остановить, если ошибка
+//     if (err) throw err;
+
+//     console.log('api server started');
+//   })
+// })
