ede1a98c3d55a363b06ee8fa7a65b7fa02ca0558
Leleath <qleleath@gmail.com>
2022-09-23 23:57:18 +1000

    r4

--- routes.js
+++ routes.js
@@ -4,27 +4,55 @@ const auth = require('./middleware/auth');
 const crypto = require('crypto');
 const bodyParser = require('body-parser').json();
 const md5 = require('md5');
+const Parse = require('parse');
+Parse.initialize(process.env.DBAPPID, process.env.DBJSKEY);
 
 const APIURL = '/api';
 const MD5KEY = process.env.MD5KEY;
-const TOKENKEY = "todo";
+const TOKENKEY = process.env.MD5KEY;
 
 module.exports = function(app) {
 
     app.get(APIURL + '/users', auth, (req, res) => {
         res.json({message:'users'});
-        // let sql = 'SELECT * FROM users';
-        // console.log("get users")
-        // db.query(sql, (err, result, fields) => {
-        //     if (err) throw err;
+        let sql = 'SELECT * FROM users';
+        console.log("get users")
+        db.query(sql, (err, result, fields) => {
+            if (err) throw err;
             
-        //     console.log("get users list")
+            console.log("get users list")
 
-        //     return res.status(200).json(result);
-        // });
+            return res.status(200).json(result);
+        });
     });
     
-    app.post(APIURL + '/users/login', bodyParser, (req, res) => {
+    app.post(APIURL + '/users/login', bodyParser, async (req, res) => {
+        let infoUser = req.body;
+  
+        try{
+            let user = await Parse.User.logIn(infoUser.usernameLogin, infoUser.passwordLogin);
+            let head = Buffer.from(
+                JSON.stringify({ alg: 'HS256', typ: 'jwt' })
+            ).toString('base64');
+    
+            let body = Buffer.from(JSON.stringify(user)).toString('base64');
+    
+            let signature = crypto
+                .createHmac('SHA256', TOKENKEY)
+                .update(head + '.' + body)
+                .digest('base64');
+
+            return res.status(200).json({
+                id: user.id,
+                username: user.username,
+                token: `${head}.${body}.${signature}`,
+            })
+            // res.render('index', { loginMessage: "User logged!", RegisterMessage: '', typeStatus: "success",  infoUser: infoUser });
+        } catch (error){
+            return res.status(400).json({message: "user not found"});
+            // res.render('index', { loginMessage: error.message, RegisterMessage: '', typeStatus: "danger",  infoUser: infoUser});
+        }
+
         // const username = req.body.username;
         // const password = md5(req.body.password + MD5KEY);
 
